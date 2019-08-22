619e3e5d86aef9e63401772e35890e5184d09d9d
Kevin Sapper <ksapper.extern@airplus.com>
2019-08-22 11:54:23 +0200

    Problem: On mobile we need to scroll the TOC until content is visible
    Solution: Move the TOC on mobile and tablet into a separate menu
    that can be display by clicking the TOC button on the top left.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,62 @@
-function navbarBurger() {
-  $(".navbar-burger").click(function() {
-    $(".navbar-burger").toggleClass("is-active");
-    $(".navbar-menu").toggleClass("is-active");
-  });
-}
+var page = (function () {
+    var html, navbarBurger, navbarMenu;
 
-$(function() {
-  navbarBurger();
-});
+    $(document).ready(function () {
+        html = $('html');
+        navbarBurger = $(".navbar-burger");
+        navbarMenu = $(".navbar-menu");
+
+        resetTheView();
+
+        window.addEventListener('resize', resetTheView);
+        window.addEventListener('scroll', resetTheView);
+        window.addEventListener('keydown', handleKeystrokes);
+
+        document.onunload = function(){
+            window.removeEventListener('resize', resetTheView);
+            window.removeEventListener('scroll', resetTheView);
+            window.removeEventListener('keydown', handleKeystrokes);
+        };
+    });
+
+    function resetTheView() {
+        if (html.hasClass('open-toc')) {
+            toggleToc();
+        }
+        if (navbarBurger.hasClass("is-active")) {
+            toggleNavbarMenu();
+        }
+    }
+
+    function tocWasClicked(e) {
+        var target = $(e.target);
+        var docsToc = $("#docsToc");
+        return (target[0] === docsToc[0] || target.parents("#docsToc").length > 0);
+    }
+
+    function listenForTocClick(e) {
+        if (!tocWasClicked(e)) toggleToc();
+    }
+
+    function toggleToc() {
+        html.toggleClass('open-toc');
+
+        setTimeout(function () {
+            if (html.hasClass('open-toc')) {
+                window.addEventListener('click', listenForTocClick);
+            } else {
+                window.removeEventListener('click', listenForTocClick);
+            }
+        }, 100);
+    }
+
+    function toggleNavbarMenu() {
+        navbarBurger.toggleClass("is-active");
+        navbarMenu.toggleClass("is-active");
+    }
+
+    return {
+        toggleToc: toggleToc,
+        toggleNavbarMenu: toggleNavbarMenu
+    };
+})();
